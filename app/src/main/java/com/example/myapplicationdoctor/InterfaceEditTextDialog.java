7d3638b2d6f041f5c16ff7be61562d1b3a2ba06e
bertrand751979 <bertrandassinou@gmail.com>
2023-03-05 12:59:40 +0100

    V3

---
+++ app/src/main/java/com/example/myapplicationdoctor/InterfaceEditTextDialog.java
@@ -0,0 +1,8 @@
+package com.example.myapplicationdoctor;
+
+import com.example.myapplicationdoctor.model.SkillDoctor;
+import com.example.myapplicationdoctor.model.UserDoctor;
+
+public interface InterfaceEditTextDialog {
+    void transfertEditText(String searchDoctor);
+}
---
+++ app/src/main/java/com/example/myapplicationdoctor/InterfaceListSpinner.java
@@ -0,0 +1,18 @@
+package com.example.myapplicationdoctor;
+
+import com.example.myapplicationdoctor.model.MyDrSkillSpinner;
+
+import java.util.ArrayList;
+
+public interface InterfaceListSpinner {
+    void interfaceSpinner(ArrayList<MyDrSkillSpinner> myDrSkillSpinner);
+}
+
+
+
+
+
+
+
+
+
